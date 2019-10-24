48035614d300aa36fbc9671dcc2b797a938af0c6
Zack Siri <zacksiri@gmail.com>
2019-10-24 13:51:50 +0700

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Eventful.MixProject do
   def project do
     [
       app: :eventful,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
